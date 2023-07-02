6bde1601db2b507fd14aa8cc8182d3fa0707f5e1
Haris Samingan <muhamad_haris@hotmail.com>
2023-07-02 20:40:34 +0800

    Add auth page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,5 @@
-import { Col, Image, Row } from "react-bootstrap";
+import AuthPage from "./pages/AuthPage";
 
 export default function App() {
-  const loginImage = "https://sig1.co/img-twitter-1";
-  return (
-    <Row>
-      <Col sm={6}>
-        <Image src={loginImage} fluid />
-      </Col>
-      <Col sm={6}>Login</Col>
-    </Row>
-  );
+  return <AuthPage />;
 }
---
+++ src/pages/AuthPage.jsx
@@ -0,0 +1,13 @@
+import { Col, Image, Row } from "react-bootstrap";
+
+export default function AuthPage() {
+  const loginImage = "https://sig1.co/img-twitter-1";
+  return (
+    <Row>
+      <Col sm={6}>
+        <Image src={loginImage} fluid />
+      </Col>
+      <Col sm={6}>Login</Col>
+    </Row>
+  );
+}
