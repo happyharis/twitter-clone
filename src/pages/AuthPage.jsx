0bc745c0bba61d19e19fed11bfa6021c403d8410
Haris Samingan <muhamad_haris@hotmail.com>
2023-07-03 21:11:05 +0800

    Add auth page

--- src/pages/AuthPage.jsx
+++ src/pages/AuthPage.jsx
@@ -6,9 +6,11 @@ export default function AuthPage() {
   const loginImage = "https://sig1.co/img-twitter-1";
   const url =
     "https://auth-back-end-sigmaschooltech.sigma-school-full-stack.repl.co";
-  const [show, setShow] = useState(false);
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
+
+  // Possible values: null (no modal shows), "Login", "SignUp"
+  const [modalShow, setModalShow] = useState(null);
+  const handleShowSignUp = () => setModalShow("SignUp");
+  const handleShowLogin = () => setModalShow("Login");
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
@@ -21,6 +23,16 @@ export default function AuthPage() {
       console.error(error);
     }
   };
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post(`${url}/login`, { username, password });
+      console.log(res.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  const handleClose = () => setModalShow(null);
   return (
     <Row>
       <Col sm={6}>
@@ -46,7 +58,7 @@ export default function AuthPage() {
             <i className="bi bi-apple"></i> Sign up with Apple
           </Button>
           <p style={{ textAlign: "center" }}>or</p>
-          <Button className="rounded-pill" onClick={handleShow}>
+          <Button className="rounded-pill" onClick={handleShowSignUp}>
             Create an account
           </Button>
           <p style={{ fontSize: "12px" }}>
@@ -57,16 +69,30 @@ export default function AuthPage() {
           <p className="mt-5" style={{ fontWeight: "bold" }}>
             Already have an account?
           </p>
-          <Button className="rounded-pill" variant="outline-primary">
+          <Button
+            className="rounded-pill"
+            variant="outline-primary"
+            onClick={handleShowLogin}
+          >
             Sign in
           </Button>
         </Col>
-        <Modal show={show} onHide={handleClose} centered>
+        <Modal
+          show={modalShow !== null}
+          onHide={handleClose}
+          animation={false}
+          centered
+        >
           <Modal.Body>
             <h2 className="mb-4" style={{ fontWeight: "bold" }}>
-              Create your account
+              {modalShow === "SignUp"
+                ? "Create your account"
+                : "Log in to your account"}
             </h2>
-            <Form className="d-grid gap-2 px-5" onSubmit={handleSignUp}>
+            <Form
+              className="d-grid gap-2 px-5"
+              onSubmit={modalShow === "SignUp" ? handleSignUp : handleLogin}
+            >
               <Form.Group className="mb-3" controlId="formBasicEmail">
                 <Form.Control
                   onChange={(e) => setUsername(e.target.value)}
@@ -93,7 +119,7 @@ export default function AuthPage() {
               </p>
 
               <Button className="rounded-pill" type="submit">
-                Sign up
+                {modalShow === "SignUp" ? "Sign up" : "Log in"}
               </Button>
             </Form>
           </Modal.Body>
